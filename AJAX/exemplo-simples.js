626adfc2c550a87d99fb489bebbdbb6e7e52e5fa
Composta Puerta <adkadkwkq12@gmail.com>
2023-12-13 22:27:43 -0500

    upload

---
+++ AJAX/exemplo-simples.js
@@ -0,0 +1,26 @@
+// Quando a página estiver totalmente carregada, chama essa função.
+window.onload = function() { 
+    document.getElementById('btn').addEventListener('click', btn_click)
+}
+
+function btn_click() {
+    // document.getElementById('caixa').innerHTML = 'Este é o texto alterado.'
+
+    // AJAX
+    let pedido = new XMLHttpRequest() // Objeto request
+
+    pedido.onreadystatechange = function() { // Função que vai atuar mediante a resposta
+        if (this.readyState == 4 && this.status == 200) {
+            document.getElementById('caixa').innerHTML = this.responseText
+        } else {
+            document.getElementById('caixa').innerHTML = 'Tente mais tarde.'
+        }
+    }
+
+    // Prepara o pedido
+    // pedido.open('GET', 'exemplo-simples.txt', true)
+    pedido.open('GET', 'exemplo-simples.php?n=lucas&s=magalhaes', true);
+    
+    // Envia o pedido
+    pedido.send()
+}
---
+++ README.md
@@ -0,0 +1,12 @@
+<strong>JavaScript - AJAX</strong> (<strong>A</strong>synchronous <strong>J</strong>avaScript e <strong>X</strong>ML)
+
+<ol>
+    <li>It is not a programming language;</li> 
+    <li>JavaScript programming technique;</li> 
+    <li>Receive and send data to the server asynchronously; e</li> 
+    <li>Through the DOM, it updates the web page without having to reload.</li>
+</ol>
+
+- [jQuery CDN](http://code.jquery.com/) <br>
+- [w3schools - Learn AJAX](https://www.w3schools.com/xml/ajax_intro.asp) <br>
+- [Laragon](https://laragon.org/) <br>
